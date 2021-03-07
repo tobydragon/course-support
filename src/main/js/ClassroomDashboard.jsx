cf4bededf62d15eea7a088af1fd99341b82d2011
Toby Dragon <dr.toby.dragon@gmail.com>
2021-03-07 14:23:20 -0500

    implemented random student chooser

--- src/main/js/ClassroomDashboard.jsx
+++ src/main/js/ClassroomDashboard.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { AttendanceChecker } from "./AttendanceChecker";
+import { SingleStudentSelector } from "./SingleStudentSelector";
 
 export const presentListFromRosterMap = (rosterMap) => {
-    return Array.from(rosterMap).filter( mapEntry => (mapEntry[1] == "present")).map(mapEntry=>mapEntry[0]);
+    return Array.from(rosterMap).filter( mapEntry => (mapEntry[1] === "present")).map(mapEntry=>mapEntry[0]);
 }
 
 export const ClassroomDashboard = (props) => {
@@ -26,6 +27,7 @@ export const ClassroomDashboard = (props) => {
         <div>
             <AttendanceChecker roster={roster} switchStudentStatus={switchStudentStatus}/>
             {Array.from (roster, ([studentName, status])=>(studentName+":" +status + " "))}
+            <SingleStudentSelector studentsPresent={presentListFromRosterMap(roster)} />
         </div>
     );
 };
