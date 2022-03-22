178833eddb63521211d9163f37267ce284899e6c
Toby Dragon <dr.toby.dragon@gmail.com>
2022-03-22 12:33:40 -0400

    Updated banner

--- src/main/js/CourseSupportHeader.jsx
+++ src/main/js/CourseSupportHeader.jsx
@@ -2,14 +2,10 @@ import { Col, Row, Jumbotron, Image } from "react-bootstrap";
 
 export const CourseSupportHeader = (props) => {
     return (
-        <Jumbotron>
-            <Row>
-                <Col className="align-items-center">
-                    <Row className="justify-content-center align-items-center">
-                            <Image src="./teacherIcon128.png" alt="" />
-                            <h1>Course Support </h1>
-                    </Row>
-                </Col>
+        <Jumbotron> 
+            <Row className="justify-content-center align-items-center">
+                    <Image src="./teacherIcon128.png" alt="" />
+                    <h1>DragonLab's Course Support </h1>
             </Row>
         </Jumbotron>
     );
